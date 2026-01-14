6e735bdfd2125e4f0f6bba7ff32f47cfdee21c11
Sasha Bates <sashabates@Sashas-MacBook-Air.local>
2026-01-14 10:34:31 -0700

    Init bouncing circles 1

---
+++ Desktop/3600/bouncingcircles1/main.js
@@ -0,0 +1,17 @@
+// bouncing circles 1
+
+/*
+
+- web gs can be done inline or in a separate file. separate files is better
+- vertex and fragment shader are parts of the whole process 
+
+- CPU
+    - RAM; JS, WebGL API within JS
+
+- GPU
+    - VRAM; GLSL, receive WebGL API commands
+    
+
+*/
+
+
